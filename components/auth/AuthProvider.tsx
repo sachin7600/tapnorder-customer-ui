2e560e3a5f77c84a06cccc1c65b0ceb87a518a1c
sachin7600 <sachin.upadhyay@ghanshyamdigital.com>
2025-10-13 19:11:40 +0530

    - Initial setup completed.

---
+++ components/auth/AuthProvider.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthProvider = ({children}: {children: React.ReactNode }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+export default AuthProvider;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
-import type { NextConfig } from "next";
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
+});
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = withPWA(nextConfig);
