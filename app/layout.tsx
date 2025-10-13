60ef60e7747cb17b391413f6a184f35f58b750ce
sachin7600 <sachin.upadhyay@ghanshyamdigital.com>
2025-10-13 19:30:22 +0530

    - Sonner toast issue fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner"
+// import { Toaster } from "@/components/ui/sonner"
 import AuthProvider from "@/components/auth/AuthProvider";
 
 const geistSans = Geist({
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <Toaster richColors/>
+      {/*<Toaster richColors/>*/}
         <AuthProvider>
           {children}
         </AuthProvider>
